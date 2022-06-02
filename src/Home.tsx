8bf0a3b6cc4d626bfd3f5f0373a0e01f02da7205
David Huber <dave@yomi.eu>
2022-06-02 21:00:33 +0200

    Add form to home page

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,26 +1,64 @@
 import { useNavigate } from "react-router-dom";
+import { ethers } from "ethers";
+import { useState, useRef } from "react";
 import "./App.css";
 
 function Home() {
   const navigate = useNavigate();
+  const [tokenAddress, setTokenAddress] = useState(
+    "0x1cA15CCdd91b55CD617a48dC9eEFb98CAe224757"
+  );
+  const [ens, setEns] = useState("auth.cxkoda.eth");
+
+  const handleVerifier = (event: any) => {
+    event.preventDefault();
+
+    if (tokenAddress !== "" && !ethers.utils.isAddress(tokenAddress)) {
+      alert("Invalid token contract address!");
+      return;
+    }
+
+    navigate("/verifier/" + tokenAddress);
+  };
+
+  const handleProver = (event: any) => {
+    event.preventDefault();
+    navigate("/prover/" + ens);
+  };
+
   return (
     <div>
-      <input
-        type="button"
-        value="Verifier"
-        className="goto-button"
-        onClick={() => {
-          navigate("/verifier");
-        }}
-      />
-      <input
-        type="button"
-        value="Prover"
-        className="goto-button"
-        onClick={() => {
-          navigate("/prover");
-        }}
-      />
+      <form onSubmit={handleVerifier}>
+        <label>
+          Token address:
+          <input
+            name="tokenAddress"
+            type="text"
+            value={tokenAddress}
+            onChange={(e: any) => {
+              setTokenAddress(e.target.value);
+            }}
+          />
+        </label>
+
+        <button type="submit">Open verifier</button>
+      </form>
+
+      <form onSubmit={handleProver}>
+        <label>
+          Auth ENS:
+          <input
+            name="ens"
+            type="text"
+            value={ens}
+            onChange={(e: any) => {
+              setEns(e.target.value);
+            }}
+          />
+        </label>
+
+        <button type="submit">Open prover</button>
+      </form>
     </div>
   );
 }
